936e01c7b482f971f186643140a3cf8fe0dffa2d
Hugo Bedard <hugobdard@gmail.com>
2017-10-10 19:05:45 -0400

    Use dataset F as ground truth

--- camera_alignment/tests/compare_epipolar_lines.m
+++ camera_alignment/tests/compare_epipolar_lines.m
@@ -6,16 +6,19 @@ dataset_name = 'Arch';
 [img_L, img_R] = load_dataset_inputs(dataset_name);
 
 % load dataset feature points
-[~, ~, ~, pts_L, pts_R, ~, ~] = load_dataset_outputs(dataset_name);
+[F_truth, ~, ~, pts_L, pts_R, ~, ~] = load_dataset_outputs(dataset_name);
 
 % estimate fundamental matrix parameters and eliminate outliers
 [F, params, inliers] = estimate_rig_fundamental_matrix(pts_L, pts_R, size(img_L));
 
-% ground truth
-F_truth = estimateFundamentalMatrix(pts_L, pts_R);
+figure;
 
 % estimation
+subplot(2, 1, 1);
 show_epilines(img_L, img_R, F, pts_L(inliers, :), pts_R(inliers, :));
+title('Epipolar Lines Estimation (Left/Right)');
 
 % ground truth
-show_epilines(img_L, img_R, F_truth, pts_L(inliers, :), pts_R(inliers, :));
+subplot(2, 1, 2);
+show_epilines(img_L, img_R, F_truth', pts_L(inliers, :), pts_R(inliers, :));
+title('Epipolar Lines Ground Truth (Left/Right)');
--- camera_alignment/tests/utils/show_epilines.m
+++ camera_alignment/tests/utils/show_epilines.m
@@ -1,21 +1,17 @@
 function show_epilines(img_L, img_R, F, pts_L, pts_R)
 
+[height, width] = size(img_L);
+
 lines_L = epipolarLine(F', pts_R);
 lines_R = epipolarLine(F, pts_L);
 border_pts_L = lineToBorderPoints(lines_L, size(img_L));
 border_pts_R = lineToBorderPoints(lines_R, size(img_R));
 
-figure;
-subplot(1, 2, 1);
-imshow(img_L); hold on;
+imshow(horzcat(img_L, img_R)); hold on;
 plot(pts_L(:, 1), pts_L(:, 2), 'go'); hold on;
-line(border_pts_L(:,[1,3])', border_pts_L(:,[2,4])');
-title('Inliers and Epipolar Lines (Left Image)');
+plot(width + pts_R(:, 1), pts_R(:, 2), 'go'); hold on;
+line(border_pts_L(:,[1,3])', border_pts_L(:,[2,4])'); hold on;
+line(width + border_pts_R(:,[1,3])', border_pts_R(:,[2,4])'); hold on;
 
-subplot(1, 2, 2);
-imshow(img_R); hold on;
-plot(pts_R(:, 1), pts_R(:, 2), 'go'); hold on;
-line(border_pts_R(:,[1,3])', border_pts_R(:,[2,4])');
-title('Inliers and Epipolar Lines (Right Image)');
 
 end
